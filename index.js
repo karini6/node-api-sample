a2687138b1746dfa925803bf7f4759a576c79579
Karianne Dramdal <karianne.dramdal@live.no>
2023-12-10 14:55:39 +0100

    Bare-bones node api w mockdata

---
+++ index.js
@@ -0,0 +1,77 @@
+// const data = require('./mockData')
+// const mockData = require('./data.js')
+
+const mockData = [
+    {
+        "title": "Test blogpost 1",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "created": "",
+        "id": 1
+    },
+    {
+        "title": "Test blogpost 1",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "created": "",
+        "id": 2
+    }
+]
+const cors = require('cors');
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+app.get('/', (req, res) => {
+    res.send("hello world!")
+})
+
+app.get('/api/blogposts', (req, res) => {
+    // if (!req.body.title || !req.body.text) res.status(400).send("Title and text is mandatory.")
+    res.send(mockData)
+})
+
+app.get('/api/blogposts/:id', (req, res) => {
+    const post = mockData.find(c => c.id === parseInt(req.params.id))
+    if(!post) res.status(404).send('No post with this ID was found.')
+    res.send(post)
+})
+
+app.put('/api/blogposts/:id', (req, res) => {
+    const post = mockData.find(c => c.id === parseInt(req.params.id))
+    if(!post) res.status(404).send('No post with this ID was found.')
+    //TODO: add validation
+
+    post.title = req.body.title
+    post.text = req.body.text
+    res.send(post)
+})
+
+app.post('/api/blogposts', (req, res) => {
+    console.log("req: ", req)
+    console.log("req.body: ", req.body)
+    const post = {
+        id: mockData.length + 1,
+        title: req.body.title,
+        text: req.body.text,
+        created: ""
+    }
+    mockData.push(post)
+    console.log("mockdata post: ", mockData)
+    res.send(post)
+})
+
+app.delete('/api/blogposts/:id', (req, res) => {
+    const post = mockData.find(c => c.id === parseInt(req.params.id))
+    if(!post) res.status(404).send('No post with this ID was found.')
+
+    const index = mockData.indexOf(post)
+    mockData.splice(index, 1)
+    res.send(post)
+})
+
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`Listening on ${port}`))
---
+++ mockData.js
@@ -0,0 +1,51 @@
+const data = [
+{
+    "title": "Test blogpost 1",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "created": "",
+    "id": 1
+},
+{
+    "title": "Test blogpost 2",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "created": "",
+    "id": 2
+},    
+{
+    "title": "Test blogpost 3",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "created": "",
+    "createdBy": "",
+    "id": 3
+},
+{
+    "title": "Test blogpost 4",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "created": "",
+    "id": 4
+},
+{
+    "title": "Test blogpost 5",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "created": "",
+    "id": 5
+},
+{
+    "title": "Test blogpost 6",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "created": "",
+    "id": 6
+},
+{
+    "title": "Test blogpost 7",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "created": "",
+    "id": 7
+},
+{
+    "title": "Test blogpost 8",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "created": "",
+    "id": 8
+}
+]
